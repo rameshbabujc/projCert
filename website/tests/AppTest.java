00953fb5b004535957d7198ef69a8630540f3377
Ramesh Babu <rameshbabujc@gmail.com>
2020-07-25 20:02:22 +0100

    Added WebElement Library

--- website/tests/AppTest.java
+++ website/tests/AppTest.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.chrome.ChromeDriver;
 
