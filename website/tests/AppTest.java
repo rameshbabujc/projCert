79162f289cdf5f4c65b9d7685e37ca3b3de58fc4
Ramesh Babu <rameshbabujc@gmail.com>
2020-07-25 21:45:32 +0100

    Added code section comments

--- website/tests/AppTest.java
+++ website/tests/AppTest.java
@@ -1,3 +1,7 @@
+// Selenium based test application for AppleBiteCo simplePHP Web App
+// Collated by: Ramesh Babu Jaya Chandran
+// Date: 25 July 2020
+
 import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
@@ -10,20 +14,32 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class AppTest {
        public static void main(String[] args) throws IOException, InterruptedException {
        		System.setProperty("webdiver.chrome.driver", "/usr/local/bin/chromedriver");
+
+		// Chrome Options to enable Headliness mode
+
  		ChromeOptions chromeOptions = new ChromeOptions();
  		chromeOptions.addArguments("--headless");
  		chromeOptions.addArguments("--no-sandbox");
 
 		WebDriver driver = new ChromeDriver(chromeOptions);
 
+		// URL of the webapp that runs in a docker container
+
 		driver.get("http://localhost:8080");
 
+		// Code below finds the Web Element "About Us"
+
 		WebElement aboutUsLink = driver.findElement(By.linkText("About Us"));
+
+		// Code below clicks on the link "About Us"
 
 		aboutUsLink.click();
 		
 
 		Thread.sleep(1000);
+
+		// Code below checks if there's a text 
+		// called "humour" in the About Us page
 
 		if (driver.getPageSource().contains("humour")) {
 			System.out.println("Can find text - humour");
