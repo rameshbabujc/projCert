f6b9abe825f98fe81c38f9dcfe8dcffc5a636e06
Ramesh Babu <rameshbabujc@gmail.com>
2020-07-25 17:31:30 +0100

    AppTest checked in

---
+++ website/tests/AppTest.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class AppTest {
+       public static void main(String[] args) throws IOException, InterruptedException {
+       		System.setProperty("webdiver.chrome.driver", "/usr/local/bin/chromedriver");
+ 		ChromeOptions chromeOptions = new ChromeOptions();
+ 		ChromeOptions.addArguments("--headless");
+ 		ChromeOptions.addArguments("--no-sandbox");
+
+		WebDriver driver = new ChromeDriver(chromeOptions);
+
+		driver.get("http://localhost:8080");
+
+		Thread.sleep(1000);
+
+		if (driver.getPageSource().contains("About Us")) {
+			System.out.println("Pass");
+		} else {
+			System.out.println("Fail");
+		}
+
+		driver.quit();
+	}
+}
+				
+
